986301be9ace30cd3ea3dce7987677a4f5d83e5e
Arman Mir <mirarman8583@gmail.com>
2025-08-23 13:55:55 +0600

    front page created

---
+++ script.js
@@ -0,0 +1,6 @@
+//LogInButton Funcionality
+document.getElementById('logInButton').addEventListener('click',function(e){
+    e.preventDefault()
+    console.log('log in button clicked');
+    console.log(e)
+});
