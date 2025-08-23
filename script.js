c77931fe23ea129e4c0d24f8b1a5484d0599ff65
Arman Mir <mirarman8583@gmail.com>
2025-08-23 14:16:44 +0600

    directed to home page upon matched input credentials

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 //LogInButton Funcionality
-document.getElementById('logInButton').addEventListener('click',function(e){
-    e.preventDefault()
-    console.log('log in button clicked');
-    console.log(e)
+document.getElementById("logInButton").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid Credentials");
+  }
 });
